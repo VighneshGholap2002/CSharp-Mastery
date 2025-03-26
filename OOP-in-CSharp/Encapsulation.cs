0f99190eb805e93d7b859b272f1a9ae289182a61
VighneshGholap2002 <vighneshgholap343@gmail.com>
2025-03-26 19:20:35 +0530

    Update Encapsulation.cs

--- OOP-in-CSharp/Encapsulation.cs
+++ OOP-in-CSharp/Encapsulation.cs
@@ -1,1 +1,21 @@
 
+class Person
+{
+    private string name; // Private field
+
+    public string Name  // Public property
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+        p.Name = "John Doe";
+        Console.WriteLine(p.Name);
+    }
+}
